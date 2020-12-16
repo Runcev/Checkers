9c28a51fb127584431e5bc2fe5dfbadd154bd9a2
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-12-16 19:37:40 +0200

    configured host

--- Checkers/Program.cs
+++ Checkers/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Checkers.Extensions;
 using Checkers.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,21 +14,17 @@ namespace Checkers
         static Task Main(string[] args)
         {
             using IHost host = CreateHostBuilder(args).Build();
-            
-            ExemplifyScoping(host.Services);
 
             return host.RunAsync();
-        }
-        
-        static void ExemplifyScoping(IServiceProvider services)
-        {
-            using var scope = services.CreateScope();
-
-            var test = scope.ServiceProvider.GetRequiredService<TestService>();
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                // .ConfigureAppConfiguration(appConfig =>
+                // {
+                //     appConfig.SetBasePath(Directory.GetCurrentDirectory());
+                //     appConfig.AddJsonFile("appsettings.json");
+                // })
                 .ConfigureServices(services => services.RegisterServices());
     }
 }
